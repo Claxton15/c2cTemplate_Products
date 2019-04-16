153cd10d98fd4a9880c18af28d87466f540bd419
Claxton15 <jacob.claxton76@gmail.com>
2019-04-16 22:26:28 +0000

    my site is actually doing stuff now

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+import requests
+import random
 app = Flask(__name__)
+
+
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template("index.html")
+    r=requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxresult=25&q=overwatch%20easter%20eggs&key=AIzaSyDYCJ1jsTCVm7h2jiDaind5B51uybE382U")
+    name=r.json()
+    app.logger.info(name)
+    print(name)
+    videoId=random.choice(name["items"])["id"]["videoId"]
+    return render_template("index.html", json=name, videoId=videoId, title=name["items"][0]["snippet"]["title"] )
+
+@app.route('/easteregg', methods=['GET'])
+def easteregg():
+    r=requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxresult=25&q=overwatch%20easter%20eggs&key=AIzaSyDYCJ1jsTCVm7h2jiDaind5B51uybE382U")
+    json=r.json()
+    videoId=random.choice(json["items"])["id"]["videoId"]
+    return redirect("http://www.youtube.com/watch?v=" + videoId, code=302)
+
+
+
 
 if __name__ == '__main__':
     app.run()
